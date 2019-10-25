f61fdea9347dd27bd745368eb0894fd439dc02fb
Christy Jacob <christyjacob4@gmail.com>
2019-10-26 00:30:10 +0530

    chore: disabled forgot password

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -77,11 +77,11 @@ const SignIn = ({auth}) => {
             Sign In
           </Button>
           <Grid container>
-            <Grid item xs>
+            {/* <Grid item xs>
               <Link href="#" variant="body2">
                 Forgot password?
               </Link>
-            </Grid>
+            </Grid> */}
             <Grid item>
               <Link href="/signup" variant="body2">
                 {'Don\'t have an account? Sign Up'}
